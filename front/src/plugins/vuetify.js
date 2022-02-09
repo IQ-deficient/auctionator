8ec990f52e264d32d1523e494719bafd66b2af36
IQ-deficient <gazivodavladimir@gmail.com>
2022-02-09 23:36:57 +0100

    vuetify theme colors

--- front/src/plugins/vuetify.js
+++ front/src/plugins/vuetify.js
@@ -1,7 +1,24 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#8f5782',
+                secondary: '#C0A28D',
+                tertiary: '#E9EBE2',
+                accent: '#757E93',
+                info: '#2a3136',
+                success: '#60955f',
+                warning: '#dd8527',
+                error: '#f44336'
+            }
+        }
+    },
+    icons: {
+        iconfont: 'mdi'
+    }
 });
