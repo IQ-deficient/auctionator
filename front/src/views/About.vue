4f2675106d2e87e3d7d66ea7044332c3f80908cd
IQ-deficient <gazivodavladimir@gmail.com>
2022-02-09 23:30:57 +0100

    npm install sweetalert2 (font invalid)

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -3,3 +3,23 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+
+// import swal from "sweetalert2";
+
+export default {
+  name: 'About',
+  data: () => ({
+  }),
+  created() {
+    // swal.fire({
+    //   title: 'Error!',
+    //   text: 'Do you want to continue',
+    //   icon: 'error',
+    //   confirmButtonText: 'Cool'
+    // })
+  }
+}
+
+</script>
