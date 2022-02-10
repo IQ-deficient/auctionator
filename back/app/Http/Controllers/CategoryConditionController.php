bc4174ae567d174ae765878f7a4ddcc3a2105646
IQ-deficient <gazivodavladimir@gmail.com>
2022-02-10 19:04:33 +0100

    php artisan make:model 'All' -a

---
+++ back/app/Http/Controllers/CategoryConditionController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CategoryCondition;
+use App\Http\Requests\StoreCategoryConditionRequest;
+use App\Http\Requests\UpdateCategoryConditionRequest;
+
+class CategoryConditionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCategoryConditionRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCategoryConditionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CategoryCondition  $categoryCondition
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CategoryCondition $categoryCondition)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CategoryCondition  $categoryCondition
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CategoryCondition $categoryCondition)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCategoryConditionRequest  $request
+     * @param  \App\Models\CategoryCondition  $categoryCondition
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCategoryConditionRequest $request, CategoryCondition $categoryCondition)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CategoryCondition  $categoryCondition
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CategoryCondition $categoryCondition)
+    {
+        //
+    }
+}
